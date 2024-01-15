eb7ee8d4d527b2208c1729d2bf0afdc463d50e5a
Terry <terrykeeton97@gmail.com>
2024-01-15 15:23:53 +0000

    First commit
    Initial project upload

---
+++ Core/PackageFinder.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Win32;
+using static Redistributable_Wizard.Utils.Helper;
+
+namespace Redistributable_Wizard.Core;
+
+public static class PackageFinder
+{
+    public enum RedistributableVersionX64
+    {
+        VcRedist2005,
+        VcRedist2008,
+        VcRedist2010,
+        VcRedist2012,
+        VcRedist2013,
+        VcRedist2015_2017_2019_2022
+    }
+
+    public enum RedistributableVersionX86
+    {
+        VcRedist2005,
+        VcRedist2008,
+        VcRedist2010,
+        VcRedist2012,
+        VcRedist2013,
+        VcRedist2015_2017_2019_2022
+    }
+    
+    /// <summary>
+    /// Check if a specific Microsoft Redistributable Package is installed on the system
+    /// </summary>
+    /// <typeparam name="TVersion">The type of the version key used to identify the package.</typeparam>
+    /// <param name="version">The version key of the package to be checked.</param>
+    /// <returns>True if the package is installed; otherwise, false.</returns>
+    /// <exception cref="Exception">Thrown when an error occurs during registry access.</exception>
+    public static bool IsPackageInstalled<TVersion>(TVersion version)
+    {
+        if (version == null) return false;
+        if (!RedistributableKeys.TryGetValue(version, out var registryInfo)) return false;
+        
+        try
+        {
+            var registryView = registryInfo.Arch == Architecture.X64 ? RegistryView.Registry64 : RegistryView.Registry32;
+            using var baseKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, registryView);
+            using var subKey = baseKey.OpenSubKey(registryInfo.RegistryKey);
+            var subKeyValue = subKey?.GetValue("DisplayName")?.ToString().ToLower();
+            if (subKeyValue != null && subKeyValue.Contains(registryInfo.DisplayName.ToLower()))
+                return true;
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+
+        return false;
+    }
+
+    /// <summary>
+    /// Checks for missing Visual C++ Redistributable packages in the system.
+    /// </summary>
+    /// <returns>
+    /// A list of display names for the missing packages.
+    /// If all packages are installed, returns a list with a single entry notifying that all packages are installed.
+    /// </returns>
+    public static List<string> GetAllMissingPackages()
+    {
+        var missingPackages = (from package in RedistributableKeys where !IsPackageInstalled(package.Key) select package.Value.DisplayName).ToList();
+
+        if (missingPackages.Count == 0)
+            missingPackages.Add("All packages are installed");
+
+        return missingPackages;
+    }
+
+    /// <summary>
+    /// Gets a list of all the current installed Visual C++ Redistributable packages on the system.
+    /// </summary>
+    /// <returns>
+    /// A list of display names for the installed packages.
+    /// </returns>
+    public static List<string> GetAllInstalledPackages()
+    {
+        return (from package in RedistributableKeys where IsPackageInstalled(package.Key) select package.Value.DisplayName).ToList();
+    }
+}
---
+++ Utils/Helper.cs
@@ -0,0 +1,30 @@
+﻿using Redistributable_Wizard.Core;
+
+namespace Redistributable_Wizard.Utils;
+
+public static class Helper
+{
+    internal enum Architecture
+    {
+        X64,
+        X86
+    }
+
+    internal static readonly Dictionary<object, (string RegistryKey, string DisplayName, Architecture Arch)> RedistributableKeys = new()
+    {
+            { PackageFinder.RedistributableVersionX64.VcRedist2005, (@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{ad8a2fa1-06e7-4b0d-927d-6e54b3d31028}", "Microsoft Visual C++ 2005 Redistributable (x64)", Architecture.X64) }, //fine
+            { PackageFinder.RedistributableVersionX64.VcRedist2008, (@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{5FCE6D76-F5DC-37AB-B2B8-22AB8CEDB1D4}", "Microsoft Visual C++ 2008 Redistributable - x64", Architecture.X64) }, //fine
+            { PackageFinder.RedistributableVersionX64.VcRedist2010, (@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{1D8E6291-B0D5-35EC-8441-6616F567A0F7}", "Microsoft Visual C++ 2010  x64 Redistributable", Architecture.X64) }, //fine - note: double white space in registry response ????
+            { PackageFinder.RedistributableVersionX64.VcRedist2012, (@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{ca67548a-5ebe-413a-b50c-4b9ceb6d66c6}", "Microsoft Visual C++ 2012 Redistributable (x64)", Architecture.X86) }, //changed, check again
+            { PackageFinder.RedistributableVersionX64.VcRedist2013, (@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{042d26ef-3dbe-4c25-95d3-4c1b11b235a7}", "Microsoft Visual C++ 2013 Redistributable (x64)", Architecture.X86) }, //changed, check again
+            { PackageFinder.RedistributableVersionX64.VcRedist2015_2017_2019_2022, (@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{1de5e707-82da-4db6-b810-5d140cc4cbb3}", "Microsoft Visual C++ 2015-2022 Redistributable (x64)", Architecture.X86) }, //changed check again
+
+            
+            { PackageFinder.RedistributableVersionX86.VcRedist2005, (@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{710f4c1c-cc18-4c49-8cbf-51240c89a1a2}", "Microsoft Visual C++ 2005 Redistributable", Architecture.X86) }, //fine
+            { PackageFinder.RedistributableVersionX86.VcRedist2008, (@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{9BE518E6-ECC6-35A9-88E4-87755C07200F}", "Microsoft Visual C++ 2008 Redistributable - x86", Architecture.X86) }, //fine
+            { PackageFinder.RedistributableVersionX86.VcRedist2010, (@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{F0C3E5D1-1ADE-321E-8167-68EF0DE699A5}", "Microsoft Visual C++ 2010  x86 Redistributable", Architecture.X86) }, //fine - note: double white space in registry response wtf???
+            { PackageFinder.RedistributableVersionX86.VcRedist2012, (@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{33d1fd90-4274-48a1-9bc1-97e33d9c2d6f}", "Microsoft Visual C++ 2012 Redistributable (x86)", Architecture.X86) }, //fine
+            { PackageFinder.RedistributableVersionX86.VcRedist2013, (@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{9dff3540-fc85-4ed5-ac84-9e3c7fd8bece}", "Microsoft Visual C++ 2013 Redistributable (x86)", Architecture.X86) }, //fine
+            { PackageFinder.RedistributableVersionX86.VcRedist2015_2017_2019_2022, (@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{2cfeba4a-21f8-4ea7-9927-c5a5c6f13cc9}", "Microsoft Visual C++ 2015-2022 Redistributable (x86)", Architecture.X86) }, //fine
+        };
+}
